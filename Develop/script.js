c5e94c1256d664fe875299c72e8ddf839d6b07e2
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-04 18:12:42 -0400

    added prompt for number selections

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 //Setting up the arrays
+//I want to tidy these up, if possible. 
+// var length = 128;
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -11,16 +13,17 @@ var confirmNumber;
 var confirmSpecialChar;
 
 //Window prompts to confirm user choices
-//not recognizing for some reasin -- figure out
-function writePassword() {
+//not recognizing for some reason -- figure out
+function generatePassword() {
   var confirmLength = window.prompt("How long would you like your password to be? You may choose between 1-128 characters.");
 }
 
-while (confirmLength <=1 || confirmLength >=129) {
-  alert("You must choose a number between 1-128")
-  var confirmLength = window.prompt("How long would you like your password to be? You may choose between 1-128 characters.");
-}  
+// while (confirmLength <=1 || confirmLength >=129) {
+//   alert("You must choose a number between 1-128")
+//   var confirmLength = window.prompt("How long would you like your password to be? You may choose between 1-128 characters.");
+// }  
   var confirmLowerCase = window.prompt("Would you like to have lower case characters? Click OK for 'yes' CANCEL for 'no'.");
+  var confirmNumber = window.prompt("Would you like to have numbers? Click OK for 'yes' CANCEL for 'no'.")
   var confirmUpperCase = window.prompt("Would you like to have upper case characters? Click OK for 'yes' CANCEL for 'no'.");
   var confirmSpecialChar = window.prompt("Would you like to have special characters? Click OK for 'yes' CANCEL for 'no'.");
 
