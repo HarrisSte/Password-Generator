943bdd7446453069bb84e9c86b5c522e254e3a65
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-04 13:54:53 -0400

    Added window prompt to PW legnth

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,17 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+var confirmLength = "";
+var confirmLowerCase = "";
+var confirmUpperCase = "";
+var confirmSpecialChar = "";
+
+//Window prompts to confirm user choice
+function generatePassword() {
+  var confirmLength = window.prompt("How long would you like your password to be? Choose 1-128 characters.");
+}
+
+var index = Math.floor(Math.random() * password.length);
 
 
 
