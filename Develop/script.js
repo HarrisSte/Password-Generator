19573ad4950fc3b01f7ad1734cd96059d4743471
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-04 16:02:41 -0400

    Worked on prompt messages, var components

--- Develop/script.js
+++ Develop/script.js
@@ -5,16 +5,48 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 var confirmLength = "";
-var confirmLowerCase = "";
-var confirmUpperCase = "";
-var confirmSpecialChar = "";
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmNumber;
+var confirmSpecialChar;
 
-//Window prompts to confirm user choice
+//Window prompts to confirm user choices
 function generatePassword() {
-  var confirmLength = window.prompt("How long would you like your password to be? Choose 1-128 characters.");
+  var confirmLength = window.prompt("How long would you like your password to be? You may choose between 1-128 characters.");
+}  
+  
+  var confirmLowerCase = window.prompt("Would you like to have lower case characters? Click OK for 'yes' CANCEL for 'no'.");
+  var confirmUpperCase = window.prompt("Would you like to have upper case characters? Click OK for 'yes' CANCEL for 'no'.");
+  var confirmSpecialChar = window.prompt("Would you like to have special characters? Click OK for 'yes' CANCEL for 'no'.");
+
+//Password generation
+var = password = [];
+
+if (confirmLowerCase) {
+  password = password.concat(lowerCase);
 }
 
-var index = Math.floor(Math.random() * password.length);
+if (confirmUpperCase) {
+  password = password.concat(upperCase);
+}
+
+if (confirmNumber) {
+  password = password.concat(number);
+}
+
+if (confirmSpecialChar) {
+  password = password.concat(specialChar);
+}
+
+console.log(password)
+
+var randomPassword = ""
+
+for (var i = 0; i < confirmLength; i++) {
+  randomPassword = randomPassword + password[Math.floor(Math.random() * password.length)];
+
+}
+
 
 
 
@@ -28,13 +60,13 @@ var index = Math.floor(Math.random() * password.length);
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
