3f761c63cfacf992126be7f2dade92377e7a598f
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-05 00:32:24 -0400

    Added func to stay w/in num. limit & cleaned code

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,65 @@
-//Setting up the arrays
-//I want to tidy these up, if possible. 
+//Setting up the arrays - I want to tidy these up, if possible. 
 
-// var length = 128;
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?','~'];
+var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?', '~'];
 
-var passwordChar=[];
+var passwordChar = [];
 var confirmLength;
 var confirmLowerCase;
 var confirmUpperCase;
 var confirmNumber;
-var confirmSpecialChar; 
+var confirmSpecialChar;
 
 //Window prompts to confirm user choices
-//not recognizing for some reason -- figure out
-function collectData() {
-  confirmLength = collectInput('How long would you like your password to be? You may choose between 8-128 characters.');
-  confirmLowerCase = collectInput("Would you like to have lower case characters? Type YES or NO.");
-  confirmNumber = collectInput("Would you like to have numbers? Click OK for 'yes' CANCEL for 'no'.")
-  confirmUpperCase = collectInput("Would you like to have upper case characters? Click OK for 'yes' CANCEL for 'no'.");
-  confirmSpecialChar = collectInput("Would you like to have special characters? Click OK for 'yes' CANCEL for 'no'.");
+function collectNumberInput(userPrompt) {
+  var response = window.prompt(userPrompt);
+  if (response >= 8 && response <= 128) {
+    return response;
+  }
+  else {
+    return 0;
+  }
 }
 
 function collectInput(userPrompt) {
-  var response = window.prompt(userPrompt);
-   return response;
+  return window.confirm(userPrompt);
+}
 
+//add stop prompt if ppl don't choose right number if confirm length =0
+function collectData() {
+  confirmLength = collectNumberInput('How long would you like your password to be? You may choose between 8-128 characters.');
+  confirmLowerCase = collectInput("Would you like to have lower case characters?");
+  confirmNumber = collectInput("Would you like to have numbers?")
+  confirmUpperCase = collectInput("Would you like to have upper case characters?");
+  confirmSpecialChar = collectInput("Would you like to have special characters?");
 }
 
 //Password generation
+//Make sure to add stop
 function generatePassword() {
   if (confirmLowerCase) {
     passwordChar = passwordChar.concat(lowerCase);
   }
-  
+
   if (confirmUpperCase) {
     passwordChar = passwordChar.concat(upperCase);
   }
-  
+
   if (confirmNumber) {
     passwordChar = passwordChar.concat(number);
   }
-  
+
   if (confirmSpecialChar) {
     passwordChar = passwordChar.concat(specialChar);
   }
 
-  console.log(passwordChar);
-
   var randomPassword = '';
-   for (var i = 0; i < confirmLength; i++) {
-  randomPassword = randomPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
- }
- return randomPassword;
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+  }
+  return randomPassword;
 }
 
 // Write password to the #password input
